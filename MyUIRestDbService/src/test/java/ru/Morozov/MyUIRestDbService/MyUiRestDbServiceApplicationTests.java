abe72401d19cd303121ee8db1055ffc6c1fb01ef
SA-Morozov <154570020+SA-Morozov@users.noreply.github.com>
2025-10-20 18:52:28 +0500

    Add files via upload

---
+++ MyUIRestDbService/src/test/java/ru/Morozov/MyUIRestDbService/MyUiRestDbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Morozov.MyUIRestDbService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUiRestDbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
